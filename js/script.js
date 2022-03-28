d47d8444964096b1553f45c34c17c6955fac10d0
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-03-28 18:18:32 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const instrument = "guitar";
+
+console.log(instrument);
+
+
+
+const button = document.querySelector("button.login");
+
+console.log(button);
+
+
+const instruments = [
+  {
+      type: "guitar",
+      colour: "red",
+  },
+  {
+      type: "piano",
+      colour: "black",
+  },
+];
+
+
+console.table(instruments );
+
+
+const name = "Percival";
+const age = 13;
+
+
+for (let  i = 0; i <= 5; i++) {
+  console.log(i);
+}
+
+
+const firstName = "Florence";
+
+const introduction = ` Hello, my name is` + firstName + ".";
+
+console.log(introduction);
+
+
+
+
+const title = "Big Technical Event";
+
+const whatToHave = "good time";
+
+const welcome = `Welcome! \n\n` +
+                `This`  + title + `is starting today.\n\n`  +
+                `Have a` + whatToHave + `!`;
+
+                console.log(welcome);
+
+
+
+  const car = {
+    "paint-colour": "red",
+    "number of wheels": 3
+};               
+              
+console.log(car["paint-colour"] );
+console.log(car["number of wheels"] );
+
+
+
+
+
+
+const product = {
+  name: "Chicken Lips",
+  price: 35,
+  expired: false,
+  getExpired: function myfuc(){
+    console.log(" OUt of date" + this. name);
+  }
+}
+
+
+product.getExpired()
